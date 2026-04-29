22657a9ce059f3756fda11f2923fa55ca96549af
Fidel kakuru <132678690+Kakurufidel@users.noreply.github.com>
2026-04-29 17:19:42 +0200

    model user

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('nom_complet', 'email', 'telephone', 'type_roles', 'date_inscription')
+    list_filter = ('type_roles', 'date_inscription')
+    search_fields = ('nom_complet', 'email')
+    readonly_fields = ('date_inscription',)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    # Choix pour les listes déroulantes
+    ROLES = [
+        ('parent', 'Parent'),
+        ('jumeau', 'Jumeau'),
+        ('jumelle', 'Jumelle'),
+    ]
+    
+    ETAT_CIVIL = [
+        ('celibataire', 'Célibataire'),
+        ('marie', 'Marié(e)'),
+        ('divorce', 'Divorcé(e)'),
+        ('veuf', 'Veuf/Veuve'),
+    ]
+    
+    STATUT_PRO = [
+        ('etudiant', 'Étudiant'),
+        ('salarie', 'Salarié'),
+        ('independant', 'Indépendant'),
+        ('sans_emploi', 'Sans emploi'),
+        ('autre', 'Autre'),
+    ]
+    
+    # Champs principaux obligatoires
+    nom_complet = models.CharField(max_length=100)
+    adresse = models.TextField()
+    telephone = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    
+    # Champs pour les jumeaux
+    noms_jumeaux_lies = models.TextField(blank=True, null=True, help_text="Noms des jumeaux/triplés (séparés par des virgules)")
+    
+    # Champs pour les parents
+    nom_papa = models.CharField(max_length=100, blank=True, null=True)
+    nom_maman = models.CharField(max_length=100, blank=True, null=True)
+    etat_civil = models.CharField(max_length=20, choices=ETAT_CIVIL, blank=True, null=True)
+    statut_pro = models.CharField(max_length=20, choices=STATUT_PRO, blank=True, null=True)
+    
+    # Photo et rôles
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    type_roles = models.CharField(max_length=50, help_text="parent,jumeau ou parent ou jumeau")
+    
+    # Date d'inscription
+    date_inscription = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.nom_complet
+    
+    def get_roles_list(self):
+        """Retourne la liste des rôles (ex: ['parent', 'jumeau'])"""
+        return self.type_roles.split(',')
+    
+    class Meta:
+        ordering = ['-date_inscription']
+        verbose_name = "Membre"
+        verbose_name_plural = "Membres"
