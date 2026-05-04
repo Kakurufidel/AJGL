58dbc7fd0e9f649b1fb8f813ddf56244787bc056
Fidel kakuru <132678690+Kakurufidel@users.noreply.github.com>
2026-05-04 15:17:29 +0200

    configiration

--- ajgl_project/urls.py
+++ ajgl_project/urls.py
@@ -4,11 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-ajgl/', admin.site.urls),
     path('', include('core.urls')),
 ]
 
-# Ajoutez cette ligne pour servir les fichiers médias en développement
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
